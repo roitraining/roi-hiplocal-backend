aa7ba2916333f49de3deb6c5900d56d0c4940fe3
Kevin Rattan <kevin@time2yak.com>
2019-02-20 10:46:25 -0500

    initial commit

---
+++ db.py
@@ -0,0 +1,38 @@
+from google.cloud import firestore
+from flask import abort
+import json
+import datetime
+
+HAPPENINGS = firestore.Client().collection('happenings')
+
+def get_happenings():
+    print('Retrieving happenings from firestore')
+    docs = HAPPENINGS.order_by('date_added', direction=firestore.Query.DESCENDING).get()
+    ret = []
+    for doc in docs:
+        d = doc.to_dict()
+        d['id'] = doc.id
+        ret.append(d)
+    return ret
+
+def add_happening(data, id):
+    print('adding a new happening' + id + ' to firestore')
+    data['date_added'] = datetime.datetime.now()
+    data['likes'] = 0
+    happening = HAPPENINGS.document(id).set(data)
+    return happening.id
+
+def like_happening(id):
+    print('user liked happening' + id)
+    d = _ensure_happening(id).to_dict()
+    d['likes'] +=1
+    HAPPENINGS.document(id).set(d)
+    return d['likes']
+
+def _ensure_happening(id):
+    try:
+        return HAPPENINGS.document(id).get()
+    except:
+        print('failed attempt to access happening ' + id)
+        abort(404)
+
---
+++ main.py
@@ -0,0 +1,31 @@
+import db
+import flask 
+import json
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def healthCheck():
+    return flask.jsonify([])
+
+@app.route('/happenings', methods=['GET'])
+def read_happenings():
+    print('backend service responding to request for happenings')
+    # string is default to prevent error when jsonifying python datetime
+    return json.dumps(db.get_happenings(), indent=4, sort_keys=True, default=str), 200
+
+@app.route('/happenings/add/<id>', methods=['POST'])
+def create_happening(id):
+    print('backend service adding new happening')
+    data = flask.request.form.to_dict(flat=True)
+    doc = db.add_happening(data, id)
+    return flask.jsonify({ 'id': doc }), 201
+
+@app.route("/happening/like/<id>")
+def like(id):
+    print('backend service adding like to happening')
+    return db.like_happening(id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8085, debug=True)
+
